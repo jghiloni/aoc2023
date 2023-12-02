16ddb324ce72f637963396c5dbfeddb2e2a0e10e
Josh Ghiloni <ghiloni@gmail.com>
2023-12-02 10:38:44 -0500

    Missed test checkin for d1p2

--- test/day1_test.exs
+++ test/day1_test.exs
@@ -5,4 +5,8 @@ defmodule Day1Test do
   test "part1-test" do
     assert Day1.part1("./inputs/day1_part1_test.txt") == 142
   end
+
+  test "part2-test" do
+    assert Day1.part2("./inputs/day1_part2_test.txt") == 281
+  end
 end
